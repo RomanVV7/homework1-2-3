fdd92543bab96370a8ed42c31c574b81bfc047bd
RomanVV7 <defjam89@mail.ru>
2024-06-01 10:29:22 +0700

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,6 @@
+Number_of_completed_homeworks = 12
+Number_of_hours_spents = 1.5
+Course_name = 'Python'
+Time_per_task = Number_of_completed_homeworks/Number_of_hours_spents/60
+print('Курс:',Course_name,', всего задач:',Number_of_completed_homeworks,', затрачено часов',Number_of_hours_spents,
+      'среднее время выполнения', round(Time_per_task,3),'часа' )
---
+++ homework3.py
@@ -0,0 +1,8 @@
+name = 'Роман'
+print(name, type(name))
+age = 34
+print(age, type(age))
+age = age + 1
+print(age, type(age))
+is_student = True
+print(is_student, type (is_student))
